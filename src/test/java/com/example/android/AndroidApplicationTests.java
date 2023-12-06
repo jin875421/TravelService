837270732d2b29709fc16fe7f3dc0ad42f3a1551
jin <jin875421@gmail.com>
2023-12-06 16:11:24 +0800

    更改

--- src/test/java/com/example/android/AndroidApplicationTests.java
+++ src/test/java/com/example/android/AndroidApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AndrodidApplicationTests {
+class AndroidApplicationTests {
 
     @Test
     void contextLoads() {
