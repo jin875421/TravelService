46a93ead781f256c1969430c9b1c246aee0143f0
zhoujh <1967478815@qq.com>
2024-06-05 08:08:51 +0800

    add: 添加关注分组交互 add: 添加用户额外信息记录

--- src/main/java/com/example/android/repository/UserExtraInfoRepository.java
+++ src/main/java/com/example/android/repository/UserExtraInfoRepository.java
@@ -3,7 +3,18 @@ package com.example.android.repository;
 import com.example.android.entity.UserExtraInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface UserExtraInfoRepository extends JpaRepository<UserExtraInfo, String>, JpaSpecificationExecutor<UserExtraInfo> {
     UserExtraInfo findByUserId(String userId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE UserExtraInfo u " +
+            "SET u.followGroupInfo = :#{#userExtraInfo.followGroupInfo} " +
+            "WHERE u.userId = :#{#userExtraInfo.userId}")
+    int updateUserExtraInfo(UserExtraInfo userExtraInfo);
 }
--- src/main/java/com/example/android/service/UserExtraInfoService.java
+++ src/main/java/com/example/android/service/UserExtraInfoService.java
@@ -13,12 +13,13 @@ import javax.transaction.Transactional;
 public class UserExtraInfoService {
     @Autowired
     private UserExtraInfoRepository userExtraInfoRepository;
+
     public String getGroupInfo(String userId) {
         return userExtraInfoRepository.findByUserId(userId).getFollowGroupInfo();
     }
 
     public UserExtraInfo getUserExtraInfo(String userId) {
-        if(userExtraInfoRepository.findByUserId(userId)==null){
+        if (userExtraInfoRepository.findByUserId(userId) == null) {
             // 老用户登录 数据库中没有对应数据
             UserInfo userInfo = new UserInfo(userId);
             return addUserExtraInfo(userInfo);
@@ -45,12 +46,13 @@ public class UserExtraInfoService {
      * 更新用户额外信息的方法。
      * <p>
      * 通过调用用户额外信息仓库的保存方法，来更新用户额外信息。此方法接收一个UserExtraInfo对象作为参数，
-     * 该对象包含了需要更新的用户额外信息。方法返回更新后的UserExtraInfo对象，该对象是保存操作的结果。
+     * 该对象包含了需要更新的用户额外信息。
      *
      * @param userExtraInfo 包含需要更新的用户额外信息的对象。
      * @return 更新后的UserExtraInfo对象。
      */
-    public UserExtraInfo updateUserExtraInfo(UserExtraInfo userExtraInfo) {
-        return userExtraInfoRepository.save(userExtraInfo);
+    public int updateUserExtraInfo(UserExtraInfo userExtraInfo) {
+        userExtraInfoRepository.save(userExtraInfo);
+        return 1;
     }
 }
